6b9ba247a91207acbda6064e46bcbd0e1cd3b4b4
Hebi Li <lihebi.com@gmail.com>
2017-12-05 17:46:44 -0600

    finish statement; almost finish expression

--- ll-parser.rkt
+++ ll-parser.rkt
@@ -108,24 +108,79 @@
 
 
 
-;; TODO TODO TODO
-(define (parse-labeled-statement ll) #f)
+;; labeled-statement ::= identifier ':' statement
+(define (parse-labeled-statement ll)
+  (consume ll)
+  (consume ll)
+  (parse-statement ll))
 
-(define (parse-case-statement ll) #f)
-(define (parse-default-statement ll) #f)
-(define (parse-compound-statement ll) #f)
-(define (parse-null-statement ll) #f)
-(define (parse-if-statement ll) #f)
-(define (parse-switch-statement ll) #f)
-(define (parse-while-statement ll) #f)
-(define (parse-do-statement ll) #f)
-(define (parse-for-statement ll) #f)
-(define (parse-goto-statement ll) #f)
-(define (parse-continue-statement ll) #f)
-(define (parse-break-statement ll) #f)
-(define (parse-return-statement ll) #f)
+;; this actually parse all nested case
+(define (parse-case-statement ll)
+  (let loop ([dummy #f])
+    (when (eq? (token-name (ll 0)) 'case)
+      (parse-constant-expression ll)
+      (consume ll)
+      (loop))
+    (parse-statement ll)))
 
+(define (parse-default-statement ll)
+  (consume ll)
+  (parse-statement ll))
 
+(define (parse-compound-statement ll)
+  ;; FIXME track the open and close brace
+  (consume ll)
+  (let loop ([dummy #f])
+    (when (not (eq? (token-name (ll 0)) 'r-brace))
+      (parse-statement-or-declaration ll))))
+
+(define (parse-null-statement ll)
+  (consume ll))
+
+;; FIXME scope
+(define (parse-if-statement ll)
+  (consume ll)
+  (parse-paren-expr-or-condition ll)
+  (parse-statement ll)
+  (when (eq? (token-name (ll 0)) 'else)
+    (consume ll)
+    (parse-statement ll)))
+(define (parse-switch-statement ll)
+  (consume ll)
+  (parse-paren-expr-or-condition ll)
+  (parse-statement ll))
+(define (parse-while-statement ll)
+  (consume ll)
+  (parse-paren-expr-or-condition ll)
+  (parse-statement ll))
+(define (parse-do-statement ll)
+  (consume ll)
+  (parse-statement ll)
+  ;; FIXME why use parse-expression here instead of parse-paren-expr-or-condition
+  (parse-expression ll))
+(define (parse-for-statement ll)
+  (consume ll)
+  ;; FIXME this might be a simple assignment statement
+  ;; FIXME check emptyness
+  (parse-declaration ll)
+  (parse-expression ll)
+  (parse-expression ll)
+  ;; FIXME GENERAL when consuming, check if it is expected
+  (consume ll)
+  (parse-statement ll))
+(define (parse-goto-statement ll)
+  (consume ll)
+  ;; this identifier should be returned
+  ;; FIXME not an identifier?
+  (when (eq? (token-name (ll 0)) 'identifier)
+    (consume ll)))
+(define (parse-continue-statement ll)
+  (consume ll))
+(define (parse-break-statement ll)
+  (consume ll))
+(define (parse-return-statement ll)
+  (consume ll)
+  (parse-expression ll))
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;; Expressions
@@ -177,8 +232,167 @@
 ;; (define (parse-rhs-of-binary-expression ll prec) #f)
 
 ;; TODO TODO TODO
-(define (parse-expression ll) #f)
+(define (parse-expression ll)
+  (parse-assignment-expression ll)
+  (parse-rhs-of-binary-expression ll prec-comma))
+(define (parse-assignment-expression ll)
+  (parse-cast-expression ll)
+  (parse-rhs-of-binary-expression ll prec-assignment))
 
+;; parse a binary expression, that has a precedence of at least
+;; min-prec
+(define (parse-rhs-of-binary-expression ll min-prec)
+  (let ([next-prec (get-bin-op-precedence (ll 0))])
+    (let loop ([dummy #f])
+      (when (>= next-prec min-prec)
+        ;; ternary operator
+        (when (eq? next-prec prec-conditional)
+          ;; middle
+          (parse-expression ll))
+        ;; rhs
+        (parse-cast-expression ll)
+        (let ([next-next-prec (get-bin-op-precedence (ll 1))])
+          (when (or (< next-prec next-next-prec)
+                    (and (= next-prec next-next-prec)
+                         ;; next-prec is right associative
+                         (or (eq? next-prec prec-conditional)
+                             (eq? next-prec prec-assignment))))
+            (parse-rhs-of-binary-expression)))
+        (loop #f)))))
+
+
+;; TODO in clang, this is actually either a cast, or a unary expr
+;;
+;; In clang, it seems to implement:
+;; cast-expression
+;; unary-expression
+;; postfix-expression
+;; primary-expression
+;; Reason to do this:
+;; 1. efficiency
+;; 2. parenthesized expression
+;;
+;; it seems that, in the K&R grammar:
+;; cast-expression ::= unary-expression
+;; cast-expression ::= ( type-name ) cast-expression
+;; while unary-expression has many form, e.g.
+;; unary-expression ::= postfix-expression
+;; FIXME it seems that this function takes a lot of UnConsume steps
+(define (parse-cast-expression ll)
+  (case (token-name (ll 0))
+    ['l-paren (parse-paren-expression ll)]
+    [('i-constant 'f-constant) (consume ll)]
+    ;; primary-expression ::= identifier
+    ;; unqualified-id ::= identifier
+    ;; constant ::= enumeration-constant
+    ;; I didn't find any code I can use
+    ['identifier #f]
+    ['string-literal (consume ll)]
+    [('++ '--) (begin
+                 (consume ll)
+                 (parse-cast-expression ll))]
+    [('& '* '+ '- '~ '!) (begin
+                           (consume ll)
+                           (parse-cast-expression ll))]
+    ;; I think I can simplify this
+    ['sizeof (parse-unary-expr-or-type-trait-expression ll)]
+    ['&& (begin
+           (consume ll)
+           ;; WTF
+           (consume ll))]
+    [else (begin
+            (parse-postfix-expression-suffix ll))]))
+
+;; primary-expression ::= ( expression )
+;; postfix-expression ::= ( type-name ) { initializer-list, }
+;; cast-expression ::= ( type-name ) cast-expression
+(define (parse-paren-expression ll)
+  (consume ll)
+  (if (eq? (token-name (ll 0)) 'l-brace)
+      (parse-compound-statement ll)
+      (if (type-id-in-parens? ll)
+          (begin
+            (parse-specifier-qualifier-list ll)
+            (parse-declarator ll)
+            ;; consume ')'
+            (consume ll)
+            (when (eq? (token-name (ll 0)) 'l-brace)
+              ;; initializer
+              (parse-compound-literal-expression ll))
+            (parse-cast-expression ll))
+          (begin
+            (parse-expression ll))))
+  (consume ll))
+
+;; we are at the left brace
+;; postfix-expression ::= ( type-name ) { initializer-list }
+;; postfix-expression ::= ( type-name ) { initializer-list , }
+(define (parse-compound-literal-expression ll)
+  (parse-initializer ll))
+
+;; initializer ::= assignment-expression
+;; | { ... }
+(define (parse-initializer ll)
+  (if (eq? (token-name (ll 0)) 'l-brace)
+      (parse-brace-initializer ll)
+      (parse-assignment-expression ll)))
+
+;; initializer ::= { initializer-list }
+;; initializer ::= { initializer-list , }
+;; initializer ::= { }
+(define (parse-brace-initializer ll)
+  (consume ll)
+  (let loop ([dummy #f])
+    (parse-initializer ll)
+    (when (try-consume ll 'comma)
+      (loop #f)))
+  (consume ll))
+
+;; TODO
+(define (type-id-in-parens? ll) #f)
+
+;; This is in parsedecl.cpp
+;; specifier-qualifier-list ::= type-specifier specifier-qualifier-list[opt]
+;; | type-qualifier specifier-qualifier-list[opt
+(define (parse-specifier-qualifier-list ll)
+  ;; FIXME why this is the only one?
+  (parse-declaration-specifiers ll))
+
+;; I should rename it or remove it
+;; unary-expression ::= sizeof unary-expression
+;; | sizeof ( type-name )
+(define (parse-unary-expr-or-type-trait-expression ll)
+  (consume ll)
+  (if (eq? (token-name (ll 0)) 'l-paren)
+      (parse-paren-expression ll)
+      (begin
+        (parse-specifier-qualifier-list ll)
+        (parse-declarator ll)
+        (parse-cast-expression ll))))
+
+;; [] () . -> ++ --
+(define (parse-postfix-expression-suffix ll)
+  (case (token-name (ll 0))
+    ['l-bracket (begin
+                  (consume ll)
+                  (parse-expression ll)
+                  (consume ll))]
+    ['l-paren (begin
+                (consume ll)
+                (parse-expression-list ll)
+                (consume ll))]
+    [('-> 'period) (begin
+                     ;; just consume??
+                     (consume ll))]
+    [('++ '--) (consume ll)]))
+
+(define (parse-constant-expression ll) #f)
+;; TODO difference from parse-expression
+(define (parse-paren-expr-or-condition ll) #f)
+
+
+;; TODO NOW
+(define (parse-expression-list ll) #f)
 
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
